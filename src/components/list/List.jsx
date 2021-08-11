710a7d22a536e55c5dc89748b9795dc8ef5b0cbb
Mohit <mk8802236@gmail.com>
2021-08-11 15:23:37 -0500

    list and Place Details

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import PlaceDetails from '../placeDetails/PlaceDetails';
+import useStyles from './styles.js';
 
 export default function List() {
+
+    const classes = useStyles();
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+
+    const places = [{ name: 'asjdhasjd' }, { name: 'sajkdhwo' }]
+
     return (
-        <div>
-            list
+        <div className={classes.container}>
+            <Typography variant="h4">Food & Dining around you</Typography>
+
+            <FormControl className={classes.formControl}>
+                <InputLabel >Type</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl className={classes.formControl}>
+                <InputLabel >Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+
+
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid key={i} item xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
+
         </div>
-    )
-}
+    );
+};
